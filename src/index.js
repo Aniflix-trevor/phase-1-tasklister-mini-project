589db11410d7bc013b642c72584634a9d0e1ffc5
Trevor <nyigeitrevor@gmail.com>
2025-03-14 22:13:05 +0300

    list adder

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+let form = document.getElementById("create-task-form");
+let text = document.getElementById("new-task-description");
+let but = document.getElementById("submit-button");
+let ul = document.getElementById("tasks");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let li = document.createElement("li");
+  li.innerText = text.value;
+  ul.appendChild(li);
+  text.value = "";
+
+  const deleteButton = document.createElement("button");
+
+  deleteButton.innerText = "X";
+  li.appendChild(deleteButton);
+
+  deleteButton.addEventListener("click", function (event) {
+    li.remove();
+  });
+});
