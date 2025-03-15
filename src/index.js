48fbb957cec401cabb77f667da1c312a6a6953a2
Trevor <nyigeitrevor@gmail.com>
2025-03-15 09:13:36 +0300

    list adder

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
-let form = document.getElementById("create-task-form");
-let text = document.getElementById("new-task-description");
-let but = document.getElementById("submit-button");
-let ul = document.getElementById("tasks");
+  let form = document.getElementById("create-task-form");
+  let text = document.getElementById("new-task-description");
+  let but = document.getElementById("submit-button");
+  let ul = document.getElementById("tasks");
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  let li = document.createElement("li");
-  li.innerText = text.value;
-  ul.appendChild(li);
-  text.value = "";
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let li = document.createElement("li");
+    li.innerText = text.value;
+    ul.appendChild(li);
+    text.value = "";
 
-  const deleteButton = document.createElement("button");
+    const deleteButton = document.createElement("button");
 
-  deleteButton.innerText = "X";
-  li.appendChild(deleteButton);
+    deleteButton.innerText = "X";
+    li.appendChild(deleteButton);
 
-  deleteButton.addEventListener("click", function (event) {
-    li.remove();
+    deleteButton.addEventListener("click", function (event) {
+      li.remove();
+    });
   });
 });
+
+
